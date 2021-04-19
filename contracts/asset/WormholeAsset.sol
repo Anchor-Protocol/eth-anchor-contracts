d975e64bd95290e695cf338ba3f13ec8dea5cafa
frostornge <frostornge@gmail.com>
2021-04-19 17:46:26 +0900

    feat: add wormhole asset proxy contract

--- contracts/asset/WormholeAsset.sol
+++ contracts/asset/WormholeAsset.sol
@@ -1,6 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0 <0.8.0;
 
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+import {IWormhole} from "../interfaces/IWormhole.sol";
 import {WrappedAsset} from "./WrappedAsset.sol";
 
-contract WormholeAsset is WrappedAsset {}
+contract WormholeAsset is WrappedAsset, Ownable {
+    address public wormhole;
+    address public token;
+    uint8 public targetChain;
+
+    function configure(
+        address _wormhole,
+        address _token,
+        uint8 _targetChain
+    ) public onlyOwner {
+        wormhole = _wormhole;
+        token = _token;
+        targetChain = _targetChain;
+    }
+
+    function initialized() public view returns (bool) {
+        return (wormhole != address(0x0) && token != address(0x0));
+    }
+
+    function burn(uint256 amount, bytes32 to) public override {
+        IWormhole(wormhole).lockAssets(token, amount, to, targetChain, 0, true);
+    }
+}
--- contracts/asset/WrappedAsset.sol
+++ contracts/asset/WrappedAsset.sol
@@ -1,12 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0 <0.8.0;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-interface WrappedAsset is IERC20 {
+interface WrappedAsset {
     event Burn(address indexed _sender, bytes32 indexed _to, uint256 amount);
 
     function burn(uint256 amount, bytes32 to) external;
-
-    function mint(address account, uint256 amount) external;
 }
