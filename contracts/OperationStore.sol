ca08b383dddcdac3727f1aa41a69243c854c08f6
frostornge <frostornge@gmail.com>
2021-04-21 16:03:56 +0900

    feat: opt store

--- contracts/OperationStore.sol
+++ contracts/OperationStore.sol
@@ -1,4 +1,71 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0 <0.8.0;
+pragma abicoder v2;
 
-contract OperationStore {}
+import {StdQueue} from "./utils/Queue.sol";
+
+contract OperationStore {
+    using StdQueue for StdQueue.Queue;
+
+    // operation struct
+    struct OperationInfo {
+        address addr;
+        address occupier;
+    }
+
+    function encodeOperation(OperationInfo memory info)
+        internal
+        pure
+        returns (bytes memory)
+    {
+        return abi.encodePacked(info.addr, info.occupier);
+    }
+
+    function decodeOperation(bytes memory rawInfo)
+        internal
+        pure
+        returns (OperationInfo memory)
+    {
+        (address addr, address occupier) =
+            abi.decode(rawInfo, (address, address));
+        return OperationInfo({addr: addr, occupier: occupier});
+    }
+
+    // queue
+    StdQueue.Queue public idleOperations;
+    StdQueue.Queue public failedOperations;
+    StdQueue.Queue public runningOperations;
+
+    function getIdleOperation(bool _consume)
+        public
+        returns (OperationInfo memory)
+    {
+        if (_consume) {
+            return decodeOperation(idleOperations.consume());
+        } else {
+            return decodeOperation(idleOperations.getItemAt(0));
+        }
+    }
+
+    function getFailedOperation(bool _consume)
+        public
+        returns (OperationInfo memory)
+    {
+        if (_consume) {
+            return decodeOperation(failedOperations.consume());
+        } else {
+            return decodeOperation(failedOperations.getItemAt(0));
+        }
+    }
+
+    function getRunningOperation(bool _consume)
+        public
+        returns (OperationInfo memory)
+    {
+        if (_consume) {
+            return decodeOperation(runningOperations.consume());
+        } else {
+            return decodeOperation(runningOperations.getItemAt(0));
+        }
+    }
+}
