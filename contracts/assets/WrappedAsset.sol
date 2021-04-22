11f6a6a3d4f3aef0e84cb512b64b5b6a825ac8e8
frostornge <frostornge@gmail.com>
2021-04-22 19:43:41 +0900

    feat: inherit IERC20

--- contracts/assets/WrappedAsset.sol
+++ contracts/assets/WrappedAsset.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0 <0.8.0;
 
-interface WrappedAsset {
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface WrappedAsset is IERC20 {
     event Burn(address indexed _sender, bytes32 indexed _to, uint256 amount);
 
     function burn(uint256 amount, bytes32 to) external;
