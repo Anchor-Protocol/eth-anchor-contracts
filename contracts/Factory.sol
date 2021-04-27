cff68f47b47ca9fb9536d56f9ec082c3bf90b421
frostornge <frostornge@gmail.com>
2021-04-27 20:47:52 +0900

    feat: add terra address buffer

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.6.0 <0.8.0;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {Clones} from "@openzeppelin/contracts/proxy/Clones.sol";
+import {EnumerableSet} from "@openzeppelin/contracts/utils/EnumerableSet.sol";
 
 interface OperationStandard {
     function initialize(bytes memory) external;
@@ -11,12 +12,20 @@ interface OperationStandard {
 }
 
 interface IFactory {
+    event ContractDeployed(
+        address indexed deployer,
+        address indexed instance,
+        bytes32 indexed terraAddress
+    );
+
     function build(uint256 _optId, address _controller)
         external
         returns (address);
 }
 
-contract Factory is Ownable {
+contract Factory is IFactory, Ownable {
+    using EnumerableSet for EnumerableSet.Bytes32Set;
+
     // permission
     mapping(address => bool) public permission;
 
@@ -42,16 +51,42 @@ contract Factory is Ownable {
         standards[_optId] = _operation;
     }
 
+    // terra address buffer
+    EnumerableSet.Bytes32Set private terraAddresses;
+
+    function pushTerraAddresses(bytes32[] memory _addrs) public onlyOwner {
+        for (uint256 i = 0; i < _addrs.length; i++) {
+            terraAddresses.add(_addrs[i]);
+        }
+    }
+
+    function fetchNextTerraAddress() public view returns (bytes32) {
+        return terraAddresses.at(0);
+    }
+
+    function fetchTerraAddress() private returns (bytes32) {
+        bytes32 addr = terraAddresses.at(0);
+        terraAddresses.remove(addr);
+        return addr;
+    }
+
     function build(uint256 _optId, address _controller)
         public
+        override
         returns (address)
     {
         require(isPermissioned(msg.sender), "Factory: not allowed");
 
+        bytes32 terraAddr = fetchTerraAddress();
         address instance = Clones.clone(standards[_optId]);
         bytes memory payload =
-            OperationStandard(standards[_optId]).initPayload(_controller, 0x0); // TODO: make terraAddress buffer
+            OperationStandard(standards[_optId]).initPayload(
+                _controller,
+                terraAddr
+            ); // TODO: make terraAddress buffer
         OperationStandard(instance).initialize(payload);
+
+        emit ContractDeployed(msg.sender, instance, terraAddr);
 
         return instance;
     }
