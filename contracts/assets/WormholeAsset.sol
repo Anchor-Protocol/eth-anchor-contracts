a673af0b4850b3ce85f4dc9ce62f91a3b657fce5
frostornge <frostornge@gmail.com>
2021-04-28 15:31:25 +0900

    fix: bridge lock asset operation

--- contracts/assets/WormholeAsset.sol
+++ contracts/assets/WormholeAsset.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.6.0 <0.8.0;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import {Proxy} from "@openzeppelin/contracts/proxy/Proxy.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
 
@@ -21,6 +22,8 @@ interface IWormholeAsset {
 }
 
 contract WormholeAsset is Initializable, Proxy, Ownable {
+    using SafeERC20 for IERC20;
+
     event Burn(address indexed _sender, bytes32 indexed _to, uint256 amount);
 
     address public factory;
@@ -42,14 +45,14 @@ contract WormholeAsset is Initializable, Proxy, Ownable {
     }
 
     function burn(uint256 amount, bytes32 to) public {
-        IWormhole(IWormholeAssetFactory(factory).wormhole()).lockAssets(
-            token,
-            amount,
-            to,
-            targetChain,
-            0,
-            true
-        );
+        IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
+
+        address wormhole = IWormholeAssetFactory(factory).wormhole();
+        if (IERC20(token).allowance(address(this), wormhole) < amount) {
+            IERC20(token).safeApprove(wormhole, type(uint256).max);
+        }
+
+        IWormhole(wormhole).lockAssets(token, amount, to, targetChain, 0, true);
         emit Burn(msg.sender, to, amount);
     }
 }
