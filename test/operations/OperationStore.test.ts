8f37414416e26dd0811971e3cc809ad5d1f28657
frostornge <frostornge@gmail.com>
2021-04-29 16:47:02 +0900

    refactor: pull out enums

--- test/operations/OperationStore.test.ts
+++ test/operations/OperationStore.test.ts
@@ -17,28 +17,12 @@ import {
   filterStructFields,
   latestBlocktime,
 } from "../shared/utilities";
+import { Queue, Status } from "../utils/TypeOperationStore";
 
 chai.use(solidity);
 
 const ZERO_ADDR = "0x0000000000000000000000000000000000000000";
 const TEST_ADDR = "0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF";
-
-enum Status {
-  IDLE = 0,
-  RUNNING_AUTO = 1,
-  RUNNING_MANUAL = 2,
-  FINISHED = 3,
-  FAILED = 4,
-  RECOVERED = 5,
-  DEALLOCATED = 6,
-}
-
-enum Queue {
-  IDLE = 0,
-  RUNNING = 1,
-  FAILED = 2,
-  BLACKHOLE = 3,
-}
 
 describe("OperationStore", () => {
   const { provider } = ethers;
---
+++ test/utils/TypeOperationStore.ts
@@ -0,0 +1,16 @@
+export enum Status {
+  IDLE = 0,
+  RUNNING_AUTO = 1,
+  RUNNING_MANUAL = 2,
+  FINISHED = 3,
+  FAILED = 4,
+  RECOVERED = 5,
+  DEALLOCATED = 6,
+}
+
+export enum Queue {
+  IDLE = 0,
+  RUNNING = 1,
+  FAILED = 2,
+  BLACKHOLE = 3,
+}
