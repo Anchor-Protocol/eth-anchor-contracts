1471ae70a9eee309b5b29feb9d1b5daa618516c9
frostornge <frostornge@gmail.com>
2021-04-29 11:35:27 +0900

    test: add lifecycle test for OperationStore.sol

---
+++ test/operations/OperationStore.test.ts
@@ -0,0 +1,192 @@
+import chai, { expect } from "chai";
+import { ethers } from "hardhat";
+import { solidity } from "ethereum-waffle";
+import {
+  Contract,
+  ContractFactory,
+  BigNumber,
+  utils,
+  BigNumberish,
+} from "ethers";
+import { isCommunityResourcable, Provider } from "@ethersproject/providers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
+
+import {
+  advanceTimeAndBlock,
+  encodeParameters,
+  filterStructFields,
+  latestBlocktime,
+} from "../shared/utilities";
+
+chai.use(solidity);
+
+const ZERO_ADDR = "0x0000000000000000000000000000000000000000";
+const TEST_ADDR = "0xFFfFfFffFFfffFFfFFfFFFFFffFFFffffFfFFFfF";
+
+enum Status {
+  IDLE = 0,
+  RUNNING_AUTO = 1,
+  RUNNING_MANUAL = 2,
+  FINISHED = 3,
+  FAILED = 4,
+  RECOVERED = 5,
+  DEALLOCATED = 6,
+}
+
+enum Queue {
+  IDLE = 0,
+  RUNNING = 1,
+  FAILED = 2,
+  BLACKHOLE = 3,
+}
+
+describe("OperationStore", () => {
+  const { provider } = ethers;
+
+  let owner: SignerWithAddress;
+  let operator: SignerWithAddress;
+
+  before("setup", async () => {
+    [owner, operator] = await ethers.getSigners();
+  });
+
+  let store: Contract;
+
+  beforeEach("deploy contract", async () => {
+    const OperationStore = await ethers.getContractFactory("OperationStore");
+    store = await OperationStore.connect(owner).deploy();
+    await store.connect(owner).transferOperator(operator.address);
+  });
+
+  describe("lifecycle", () => {
+    beforeEach("alloc", async () => {
+      await expect(store.connect(owner).allocate(TEST_ADDR))
+        .to.emit(store, "OperationAllocated")
+        .withArgs(owner.address, TEST_ADDR);
+      expect(await store.getStatusOf(TEST_ADDR)).to.eq(Status.IDLE);
+      expect(await store.getAvailableOperation()).to.eq(TEST_ADDR);
+    });
+
+    describe("init", () => {
+      describe("=> finish", () => {
+        it("#autoFinish = true", async () => {
+          await expect(store.connect(operator).init(true))
+            .to.emit(store, "OperationInitialized")
+            .withArgs(operator.address, TEST_ADDR, true);
+          expect(await store.getAvailableOperation()).to.eq(ZERO_ADDR);
+          expect(await store.getRunningOperationAt(Status.IDLE)).to.eq(
+            TEST_ADDR
+          );
+          expect(await store.getStatusOf(TEST_ADDR)).to.eq(Status.RUNNING_AUTO);
+
+          await expect(store.connect(operator).finish(TEST_ADDR))
+            .to.emit(store, "OperationFinished")
+            .withArgs(operator.address, TEST_ADDR);
+          expect(await store.getStatusOf(TEST_ADDR)).to.eq(Status.FINISHED);
+
+          await expect(store.connect(operator).flush(Queue.RUNNING, 10))
+            .to.emit(store, "OperationFlushed")
+            .withArgs(operator.address, TEST_ADDR, Queue.RUNNING, Queue.IDLE);
+
+          expect(await store.getStatusOf(TEST_ADDR)).to.eq(Status.IDLE);
+          expect(await store.getAvailableOperation()).to.eq(TEST_ADDR);
+          expect(await store.getRunningOperationAt(0)).to.eq(ZERO_ADDR);
+        });
+
+        it("#autoFinish = false", async () => {
+          await expect(store.connect(operator).init(false))
+            .to.emit(store, "OperationInitialized")
+            .withArgs(operator.address, TEST_ADDR, false);
+          expect(await store.getAvailableOperation()).to.eq(ZERO_ADDR);
+          expect(await store.getRunningOperationAt(0)).to.eq(ZERO_ADDR);
+          expect(await store.getStatusOf(TEST_ADDR)).to.eq(
+            Status.RUNNING_MANUAL
+          );
+
+          await expect(store.connect(operator).finish(TEST_ADDR))
+            .to.emit(store, "OperationFinished")
+            .withArgs(operator.address, TEST_ADDR)
+            .to.emit(store, "OperationAllocated")
+            .withArgs(operator.address, TEST_ADDR);
+
+          expect(await store.getStatusOf(TEST_ADDR)).to.eq(Status.IDLE);
+          expect(await store.getAvailableOperation()).to.eq(TEST_ADDR);
+          expect(await store.getRunningOperationAt(0)).to.eq(ZERO_ADDR);
+        });
+      });
+
+      describe("=> fail", () => {
+        it("#idle", async () => {
+          await expect(store.connect(operator).fail(TEST_ADDR))
+            .to.emit(store, "OperationFailed")
+            .withArgs(operator.address, TEST_ADDR);
+          expect(await store.getAvailableOperation()).to.eq(ZERO_ADDR);
+          expect(await store.getFailedOperationAt(0)).to.eq(TEST_ADDR);
+        });
+
+        it("#running", async () => {
+          await store.connect(operator).init(true);
+          await expect(store.connect(operator).fail(TEST_ADDR))
+            .to.emit(store, "OperationFailed")
+            .withArgs(operator.address, TEST_ADDR);
+          expect(await store.getStatusOf(TEST_ADDR)).to.eq(Status.FAILED);
+
+          await expect(store.connect(operator).flush(Queue.RUNNING, 10))
+            .to.emit(store, "OperationFlushed")
+            .withArgs(operator.address, TEST_ADDR, Queue.RUNNING, Queue.FAILED);
+
+          expect(await store.getRunningOperationAt(0)).to.eq(ZERO_ADDR);
+          expect(await store.getFailedOperationAt(0)).to.eq(TEST_ADDR);
+        });
+
+        describe("#queue", () => {
+          beforeEach("setup", async () => {
+            await store.connect(operator).init(true);
+            await store.connect(operator).fail(TEST_ADDR);
+            await store.connect(operator).flush(Queue.RUNNING, 10);
+            console.log(await store.getFailedOperationAt(0));
+          });
+
+          it("=> recover", async () => {
+            await expect(store.connect(operator).recover(TEST_ADDR))
+              .to.emit(store, "OperationRecovered")
+              .withArgs(operator.address, TEST_ADDR);
+            expect(await store.getStatusOf(TEST_ADDR)).to.eq(Status.RECOVERED);
+
+            await expect(store.connect(operator).flush(Queue.FAILED, 10))
+              .to.emit(store, "OperationFlushed")
+              .withArgs(operator.address, TEST_ADDR, Queue.FAILED, Queue.IDLE);
+
+            expect(await store.getStatusOf(TEST_ADDR)).to.eq(Status.IDLE);
+            expect(await store.getFailedOperationAt(0)).to.eq(ZERO_ADDR);
+            expect(await store.getAvailableOperation()).to.eq(TEST_ADDR);
+          });
+
+          it("=> dealloc", async () => {
+            await expect(store.connect(owner).deallocate(TEST_ADDR))
+              .to.emit(store, "OperationDeallocated")
+              .withArgs(owner.address, TEST_ADDR);
+            expect(await store.getStatusOf(TEST_ADDR)).to.eq(
+              Status.DEALLOCATED
+            );
+
+            await expect(store.connect(operator).flush(Queue.FAILED, 10))
+              .to.emit(store, "OperationFlushed")
+              .withArgs(
+                operator.address,
+                TEST_ADDR,
+                Queue.FAILED,
+                Queue.BLACKHOLE
+              );
+
+            expect(await store.getStatusOf(TEST_ADDR)).to.eq(
+              Status.DEALLOCATED
+            );
+            expect(await store.getFailedOperationAt(0)).to.eq(ZERO_ADDR);
+            expect(await store.getAvailableOperation()).to.eq(ZERO_ADDR);
+          });
+        });
+      });
+    });
+  });
+});
