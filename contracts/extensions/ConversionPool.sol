b7eb0ed887f28af59e87ae14bc715aaed64873c6
frostornge <frostornge@gmail.com>
2021-05-04 14:28:05 +0900

    feat: gas optimization

--- contracts/extensions/ConversionPool.sol
+++ contracts/extensions/ConversionPool.sol
@@ -9,11 +9,15 @@ import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
 import {
     IUniswapV2Router02
 } from "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+import {
+    IUniswapV2Pair
+} from "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
 
 import {IExchangeRateFeeder} from "./ExchangeRateFeeder.sol";
 import {IRouter} from "../core/Router.sol";
 import {Operator} from "../utils/Operator.sol";
 import {IERC20Controlled, ERC20Controlled} from "../utils/ERC20Controlled.sol";
+import {UniswapV2Library} from "../libraries/UniswapV2Library.sol";
 
 interface ISwapper {
     function swapTokens(
@@ -38,13 +42,16 @@ contract ConversionPool is IConversionPool, Operator, Initializable {
     IERC20 public inputToken; // DAI / USDC / USDT
     IERC20Controlled public outputToken; // aDAI / aUSDC / aUSDT
 
+    // swap settings
+    address public swapFactory;
+    address public bridgeToken;
+
     // proxy settings
     IERC20 public proxyInputToken; // UST
     IERC20 public proxyOutputToken; // aUST
     uint256 public proxyReserve = 0; // aUST reserve
 
     IRouter public optRouter;
-    ISwapper public swapRouter;
     IExchangeRateFeeder public feeder;
 
     function initialize(
@@ -56,7 +63,7 @@ contract ConversionPool is IConversionPool, Operator, Initializable {
         address _proxyOutputToken,
         // ===== others
         address _optRouter,
-        address _swapRouter,
+        address _swapFactory,
         address _exchangeRateFeeder
     ) public initializer {
         inputToken = IERC20(_inputToken);
@@ -65,16 +72,19 @@ contract ConversionPool is IConversionPool, Operator, Initializable {
         proxyInputToken = IERC20(_proxyInputToken);
         proxyOutputToken = IERC20(_proxyOutputToken);
 
-        setSwapRouter(_swapRouter);
+        setSwapFactory(_swapFactory);
         setOperationRouter(_optRouter);
         setExchangeRateFeeder(_exchangeRateFeeder);
     }
 
     // governance
 
-    function setSwapRouter(address _swapRouter) public onlyOwner {
-        swapRouter = ISwapper(_swapRouter);
-        inputToken.safeApprove(address(swapRouter), type(uint256).max);
+    function setBridgeToken(address _bridgeToken) public onlyOwner {
+        bridgeToken = _bridgeToken;
+    }
+
+    function setSwapFactory(address _swapFactory) public onlyOwner {
+        swapFactory = _swapFactory;
     }
 
     function setOperationRouter(address _optRouter) public onlyOwner {
@@ -102,38 +112,81 @@ contract ConversionPool is IConversionPool, Operator, Initializable {
         proxyOutputToken.safeTransfer(msg.sender, _amount);
     }
 
+    // swap: fetched from Uniswap/uniswap-v2-periphery
+    function _swap(
+        uint256[] memory amounts,
+        address[] memory path,
+        address _to
+    ) private {
+        for (uint256 i; i < path.length - 1; i++) {
+            (address input, address output) = (path[i], path[i + 1]);
+            (address token0, ) = UniswapV2Library.sortTokens(input, output);
+            uint256 amountOut = amounts[i + 1];
+            (uint256 amount0Out, uint256 amount1Out) =
+                input == token0
+                    ? (uint256(0), amountOut)
+                    : (amountOut, uint256(0));
+            address to =
+                i < path.length - 2
+                    ? UniswapV2Library.pairFor(swapFactory, output, path[i + 2])
+                    : _to;
+            IUniswapV2Pair(UniswapV2Library.pairFor(swapFactory, input, output))
+                .swap(amount0Out, amount1Out, to, new bytes(0));
+        }
+    }
+
+    function _swapToken(
+        address _from,
+        address _to,
+        uint256 _amount
+    ) internal {
+        address[] memory path;
+
+        if (bridgeToken == address(0x0)) {
+            path = new address[](2);
+            path[0] = _from;
+            path[1] = _to;
+        } else {
+            path = new address[](3);
+            path[0] = _from;
+            path[1] = bridgeToken;
+            path[2] = _to;
+        }
+
+        uint256[] memory amounts =
+            UniswapV2Library.getAmountsOut(swapFactory, _amount, path);
+        require(
+            amounts[amounts.length - 1] >= 0,
+            "UniswapV2Router: INSUFFICIENT_OUTPUT_AMOUNT"
+        );
+        IERC20(path[0]).safeTransferFrom(
+            msg.sender,
+            UniswapV2Library.pairFor(swapFactory, path[0], path[1]),
+            amounts[0]
+        );
+        _swap(amounts, path, address(this));
+    }
+
     // operations
     function deposit(uint256 _amount) public override {
-        inputToken.safeTransferFrom(msg.sender, address(this), _amount);
-
         // swap to UST
-        swapRouter.swapTokens(
-            address(inputToken),
-            address(proxyInputToken),
-            _amount
-        );
+        _swapToken(address(inputToken), address(proxyInputToken), _amount);
 
         // depositStable
         uint256 ust = proxyInputToken.balanceOf(address(this));
         optRouter.depositStable(ust);
 
         uint256 pER = feeder.exchangeRateOf(address(inputToken));
-        uint256 out = ust.mul(1e18).div(pER);
-
-        outputToken.mint(out);
-        outputToken.safeTransfer(msg.sender, out);
+        outputToken.mint(msg.sender, ust.mul(1e18).div(pER));
     }
 
     function redeem(uint256 _amount) public override {
-        outputToken.safeTransferFrom(msg.sender, address(this), _amount);
-        outputToken.burn(_amount);
+        outputToken.burnFrom(msg.sender, _amount);
 
         uint256 pER = feeder.exchangeRateOf(address(inputToken));
         uint256 out = _amount.mul(pER).div(1e18);
 
         uint256 aER = feeder.exchangeRateOf(address(proxyInputToken));
-        uint256 redeemAmount = out.mul(1e18).div(aER);
-
-        optRouter.redeemStable(msg.sender, redeemAmount);
+        optRouter.redeemStable(msg.sender, out.mul(1e18).div(aER));
     }
 }
--- contracts/utils/ERC20Controlled.sol
+++ contracts/utils/ERC20Controlled.sol
@@ -5,24 +5,40 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {Context} from "@openzeppelin/contracts/utils/Context.sol";
+import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
 
 interface IERC20Controlled is IERC20 {
-    function mint(uint256 amount) external;
+    function mint(address _to, uint256 _amount) external;
 
-    function burn(uint256 amount) external;
+    function burn(uint256 _amount) external;
+
+    function burnFrom(address _from, uint256 _amount) external;
 }
 
 contract ERC20Controlled is Context, Ownable, IERC20Controlled, ERC20 {
+    using SafeMath for uint256;
+
     constructor(string memory _name, string memory _symbol)
         Ownable()
         ERC20(_name, _symbol)
     {}
 
-    function mint(uint256 amount) public override onlyOwner {
-        _mint(_msgSender(), amount);
+    function mint(address _to, uint256 _amount) public override onlyOwner {
+        _mint(_to, _amount);
     }
 
-    function burn(uint256 amount) public override {
-        _burn(_msgSender(), amount);
+    function burn(uint256 _amount) public override {
+        _burn(_msgSender(), _amount);
+    }
+
+    function burnFrom(address _from, uint256 _amount) public override {
+        uint256 decreasedAllowance =
+            allowance(_from, _msgSender()).sub(
+                _amount,
+                "ERC20: burn amount exceeds allowance"
+            );
+
+        _approve(_from, _msgSender(), decreasedAllowance);
+        _burn(_from, _amount);
     }
 }
