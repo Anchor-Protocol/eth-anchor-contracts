30cd04405d7e1407653b44914285cac58a050341
frostornge <frostornge@gmail.com>
2021-05-27 16:41:36 +0900

    feat: busd

--- scripts/contracts.ts
+++ scripts/contracts.ts
@@ -7,10 +7,10 @@ export const CONTRACTS = {
     aUST: "0xa8De3e3c934e2A1BB08B010104CcaBBD4D6293ab",
     UST: "0xa47c8bf37f92aBed4A126BDA807A7b7498661acD",
     // ether
-    WETH: "",
     DAI: "",
     USDT: "",
     USDC: "",
+    BUSD: "",
   },
   ropsten: {
     // uniswap
@@ -20,9 +20,9 @@ export const CONTRACTS = {
     aUST: "0x006479f75D6622AE6a21BE17C7F555B94c672342",
     UST: "0x6cA13a4ab78dd7D657226b155873A04DB929A3A4",
     // ether
-    WETH: "0xc778417e063141139fce010982780140aa0cd5ab",
-    DAI: "0xad6d458402f60fd3bd25163575031acdce07538d",
-    USDT: "0x516de3a7A567d81737e3a46ec4FF9cFD1fcb0136",
-    USDC: "0x0D9C8723B343A8368BebE0B5E89273fF8D712e3C",
+    DAI: "0x6bb59e3f447222b3fcf2847111700723153f625a",
+    USDT: "0x6af27a81ceb61073ccca401ca6b43064f369dc02",
+    USDC: "0xe015fd30cce08bc10344d934bdb2292b1ec4bbbd",
+    BUSD: "0xaae6df09ae0d322a666edc63e6a69e4b0fab6f5d",
   },
 } as { [network: string]: { [name: string]: string } };
