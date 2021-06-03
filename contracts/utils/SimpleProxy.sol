6a1e95a10f04325faffccc6ae6265d4a7719cefd
frostornge <frostornge@gmail.com>
2021-06-03 16:55:08 +0900

    fix: rename simple proxy contract

--- contracts/utils/SimpleProxy.sol
+++ contracts/utils/SimpleProxy.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.6.0 <0.8.0;
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {Proxy} from "@openzeppelin/contracts/proxy/Proxy.sol";
 
-contract StdProxy is Proxy, Ownable {
+contract SimpleProxy is Proxy, Ownable {
     address public impl;
 
     function setImplementation(address _impl) public onlyOwner {
