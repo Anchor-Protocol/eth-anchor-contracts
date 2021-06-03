6128bcbd4371e70d942fa74c297caee71b768581
frostornge <frostornge@gmail.com>
2021-06-03 15:15:44 +0900

    feat: curve swapper deploy script

--- scripts/deploy-curve.ts
+++ scripts/deploy-curve.ts
@@ -17,33 +17,112 @@ async function main(): Promise<void> {
 
   const Swapper = await ethers.getContractFactory("CurveSwapper");
   const swapper = await Swapper.connect(operator).deploy({ gasPrice });
+  console.log(`Deploy swapper ${swapper.deployTransaction.hash}`);
   await provider.waitForTransaction(swapper.deployTransaction.hash, 2);
+
+  console.log(swapper.address);
+  await run("verify:verify", {
+    address: swapper.address,
+    constructorArguments: [],
+  });
 
   let tx;
 
-  // // ust -> dai
-  // tx = await swapper
-  //   .connect(operator)
-  //   .setRoute(contracts.UST, contracts.DAI, [contracts.CrvUSTPool], [0, 2], {
-  //     gasPrice,
-  //   });
-  // await provider.waitForTransaction(tx.hash, 2);
+  const UST = await ethers.getContractAt("IERC20", contracts.UST);
+  tx = await UST.connect(operator).approve(
+    swapper.address,
+    constants.WeiPerEther.pow(2),
+    { gasPrice }
+  );
+  console.log(`UST.approve ${tx.hash}`);
+  await provider.waitForTransaction(tx.hash, 2);
 
-  // // ust -> usdc
-  // tx = await swapper
-  //   .connect(operator)
-  //   .setRoute(contracts.UST, contracts.USDC, [contracts.CrvUSTPool], [0, 4], {
-  //     gasPrice,
-  //   });
-  // await provider.waitForTransaction(tx.hash, 2);
+  // ust -> dai
+  tx = await swapper
+    .connect(operator)
+    .setRoute(
+      contracts.UST,
+      contracts.DAI,
+      [contracts.CrvUSTPool],
+      [contracts.UST],
+      [0, 1],
+      {
+        gasPrice,
+      }
+    );
+  console.log(`Swapper.setRoute ${tx.hash}`);
+  await provider.waitForTransaction(tx.hash, 2);
 
-  // // ust -> usdt
-  // tx = await swapper
-  //   .connect(operator)
-  //   .setRoute(contracts.UST, contracts.USDT, [contracts.CrvUSTPool], [0, 3], {
-  //     gasPrice,
-  //   });
-  // await provider.waitForTransaction(tx.hash, 2);
+  tx = await swapper
+    .connect(operator)
+    .swapToken(
+      contracts.UST,
+      contracts.DAI,
+      constants.WeiPerEther,
+      0,
+      operator.address,
+      { gasPrice }
+    );
+  console.log(`UST => DAI ${tx.hash}`);
+  await provider.waitForTransaction(tx.hash, 2);
+
+  // ust -> usdc
+  tx = await swapper
+    .connect(operator)
+    .setRoute(
+      contracts.UST,
+      contracts.USDC,
+      [contracts.CrvUSTPool],
+      [contracts.UST],
+      [0, 2],
+      {
+        gasPrice,
+      }
+    );
+  console.log(`Swapper.setRoute ${tx.hash}`);
+  await provider.waitForTransaction(tx.hash, 2);
+
+  tx = await swapper
+    .connect(operator)
+    .swapToken(
+      contracts.UST,
+      contracts.USDC,
+      constants.WeiPerEther,
+      0,
+      operator.address,
+      { gasPrice }
+    );
+  console.log(`UST => USDC ${tx.hash}`);
+  await provider.waitForTransaction(tx.hash, 2);
+
+  // ust -> usdt
+  tx = await swapper
+    .connect(operator)
+    .setRoute(
+      contracts.UST,
+      contracts.USDT,
+      [contracts.CrvUSTPool],
+      [contracts.UST],
+      [0, 3],
+      {
+        gasPrice,
+      }
+    );
+  console.log(`Swapper.setRoute ${tx.hash}`);
+  await provider.waitForTransaction(tx.hash, 2);
+
+  tx = await swapper
+    .connect(operator)
+    .swapToken(
+      contracts.UST,
+      contracts.USDT,
+      constants.WeiPerEther,
+      0,
+      operator.address,
+      { gasPrice }
+    );
+  console.log(`UST => USDT ${tx.hash}`);
+  await provider.waitForTransaction(tx.hash, 2);
 
   // ust -> usdc -> busd
   tx = await swapper
@@ -53,23 +132,24 @@ async function main(): Promise<void> {
       contracts.BUSD,
       [contracts.CrvUSTPool, contracts.CrvBUSDPool],
       [contracts.UST, contracts.USDC],
-      [0, 2, 1, 0],
+      [0, 2, 1, 3],
       { gasPrice }
     );
+  console.log(`Swapper.setRoute ${tx.hash}`);
   await provider.waitForTransaction(tx.hash, 2);
 
-  const UST = await ethers.getContractAt("IERC20", contracts.UST);
-  tx = await UST.connect(operator).approve(
-    swapper.address,
-    constants.WeiPerEther.pow(2)
-  );
+  tx = await swapper
+    .connect(operator)
+    .swapToken(
+      contracts.UST,
+      contracts.BUSD,
+      constants.WeiPerEther,
+      0,
+      operator.address,
+      { gasPrice }
+    );
+  console.log(`UST => BUSD ${tx.hash}`);
   await provider.waitForTransaction(tx.hash, 2);
-
-  console.log(swapper.address);
-  await run("verify:verify", {
-    address: swapper.address,
-    constructorArguments: [],
-  });
 }
 
 main().catch(console.error);
